eeed69f40dbc030182b199f5c07f9021c621a740
조재철 <rhsnqk@gmail.com>
2022-07-06 15:59:02 +0900

    start

---
+++ src/main/java/com/example/whyjenkins/WhyjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.whyjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyjenkinsApplication.class, args);
+	}
+
+}
